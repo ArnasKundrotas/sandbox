b7915febc93901fc689369bd9fa932f101aebe40
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-20 14:45:19 +0200

    start new project in the sandbox repository called js ex reduce

--- js ex includes/index.js
+++ js ex includes/index.js
@@ -34,3 +34,29 @@ console.log(elementIsIncluded(["flexbox", "css"], tags)); // false
 console.log(elementIsIncluded({title:"Apple",quantity:25}, fruits)); // true
 console.log(elementIsIncluded({title:"Banana"}, fruits)); // false
 console.log(elementIsIncluded(25, primitiveTypesArray)); // true
+console.log("--------------------------------");
+
+// --- --- //
+
+const myNumbers = [123, 50, 27];
+
+const pushIfUnique = (arr, newValue) => {
+    console.log("Is " + newValue + " unique?");
+    arr.includes(newValue) ? console.log(newValue + " is already in the array") : arr.push(newValue);
+}
+
+pushIfUnique(myNumbers, 50); // 50 is already in the array
+console.log(myNumbers); // [123, 50, 27]
+console.log("---/---");
+
+pushIfUnique(myNumbers, 80); // 80 is unique
+console.log(myNumbers); // [123, 50, 27, 80]
+console.log("---/---");
+
+pushIfUnique(myNumbers, 80); // 80 is already in the array
+console.log(myNumbers); // [123, 50, 27, 80]
+console.log("---/---");
+
+pushIfUnique(myNumbers, 77); // 77 is unique
+console.log(myNumbers); // [123, 50, 27, 80, 77]
+console.log("---/---");
