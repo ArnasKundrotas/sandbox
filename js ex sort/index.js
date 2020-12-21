7ea36d8b6c35da96b96b26e81ee46f651c2ad2ea
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-21 18:02:02 +0200

    update js ex sort project

--- js ex sort/index.js
+++ js ex sort/index.js
@@ -53,3 +53,77 @@ const sortProductsByPrice = products => products.sort ((a, b) => a.price - b.pri
 sortProductsByPrice(products);
 
 console.log(products);
+
+// --- --- //
+const persons = [
+    {
+        name: "Andy",
+        friendsQty: 10,
+        index: 1
+    },
+    {
+        name: "Mike",
+        friendsQty: 5,
+        index: 2
+    },
+    {
+        name: 'Sophia',
+        friendsQty: 10,
+        index: 3
+    },
+    {
+        name: 'Joushua',
+        friendsQty: 3,
+        index: 4
+    },
+    {
+        name: "John",
+        friendsQty: 10,
+        index: 5
+    },
+    {
+        name: "Gabriela",
+        friendsQty: 8,
+        index: 6
+    },
+    {
+        name: "Tyler",
+        friendsQty: 7,
+        index: 7
+    },
+    {
+        name: "Dylan",
+        friendsQty: 2,
+        index: 8
+    },
+    {
+        name: "Sarah",
+        friendsQty: 5,
+        index:9
+    },
+    {
+        name: "Alexa",
+        friendsQty: 10,
+        index: 10
+    },
+    {
+        name: "Henry",
+        friendsQty: 10,
+        index: 11
+    },
+    {
+        name: "Arianna",
+        friendsQty: 10,
+        index: 12
+    }
+
+];
+
+
+const sortPersonsByFriendsQty = persons => persons.sort ((a, b) => a.friendsQty - b.friendsQty);
+
+sortPersonsByFriendsQty(persons);
+
+console.log(persons);
+
+/* Array of persons sorted by friendsQty in ascending order */
